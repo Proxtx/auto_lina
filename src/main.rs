05b8caec25f78cd05cea21e3c9849d2ca8d1fc8c
Proxtx <gopro54122@gmail.com>
2024-08-16 19:31:30 +0200

    I think this is the best/funniest version

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,55 @@
+use std::thread;
+
 use active_win_pos_rs::get_active_window;
 use enigo::{Direction, Enigo, Key, Keyboard, Settings};
+use rand::{seq::SliceRandom, Rng};
 
 fn main() {
     let mut e = Enigo::new(&Settings::default()).unwrap();
+    let keys = [
+        Key::Unicode('w'),
+        Key::Unicode('a'),
+        Key::Unicode('s'),
+        Key::Unicode('d'),
+        Key::Space,
+    ];
+    let mut rng = rand::thread_rng();
+
+    thread::spawn(move || {
+        let mut e = Enigo::new(&Settings::default()).unwrap();
+        loop {
+            if let Ok(w) = get_active_window() {
+                if w.app_name == "MosaLina" {
+                    e.key(Key::Unicode('e'), Direction::Click).unwrap();
+                    std::thread::sleep(std::time::Duration::from_millis(10));
+                    e.key(Key::Unicode('q'), Direction::Click).unwrap();
+                }
+            }
+            std::thread::sleep(std::time::Duration::from_millis(200));
+        }
+    });
+
+    thread::spawn(move || {
+        let mut e = Enigo::new(&Settings::default()).unwrap();
+        loop {
+            if let Ok(w) = get_active_window() {
+                if w.app_name == "MosaLina" {
+                    e.key(Key::Unicode('r'), Direction::Click).unwrap();
+                }
+            }
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+    });
+
     loop {
         if let Ok(w) = get_active_window() {
-            println!("{}", w.app_name);
-            if w.app_name == "Mosa Lina" {
-                println!(" OK{}", w.app_name);
-                e.key(Key::E, Direction::Click).unwrap();
-                //simulate::press(simulate::Key::E).unwrap();
+            if w.app_name == "MosaLina" {
+                let key = *keys.choose(&mut rng).unwrap();
+                e.key(key, Direction::Press).unwrap();
+                std::thread::sleep(std::time::Duration::from_millis(rng.gen_range(0..=1000)));
+                e.key(key, Direction::Release).unwrap();
             }
         };
-        std::thread::sleep(std::time::Duration::from_secs(1))
+        std::thread::sleep(std::time::Duration::from_millis(10))
     }
 }
